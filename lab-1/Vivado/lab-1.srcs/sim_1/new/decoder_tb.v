b80afae1e27c18c4c0b569141289bc780ab3892c
e1turin <ivturnik2003@gmail.com>
2024-03-11 21:16:16 +0300

    fix issues & complete report

--- lab-1/Vivado/lab-1.srcs/sim_1/new/decoder_tb.v
+++ lab-1/Vivado/lab-1.srcs/sim_1/new/decoder_tb.v
@@ -2,54 +2,57 @@
 
 module decoder_tb;
 
-    reg [2:0] s;
-    wire[7:0] d_golden, d_test;
-    reg en;
-    integer i;
-    
-    decoder_golden decoder_goldn(
-        .s(s),
-        .en(en),
-        .d(d_golden)
-    );
-    
-    decoder decoder_test(
-        .s(s),
-        .en(en),
-        .d(d_test)
-    );
-    
-    
-    initial begin
-        for(i = 0; i < 8; i = i+1) begin
-            s = i;
-            en = 1;
-            
-            #10
+reg [2:0] s;
+wire[7:0] d_golden, d_test;
+reg en;
+integer i;
 
-            if (d_test == d_golden) begin
-                $display("OK: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
-            end 
-            else 
-            begin
-                $display("Fail: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
-                $display("Expect: s=%b, d=%b, en=%b, i=%0d", s, d_golden, en, i);
-            end
+decoder_golden decoder_golden(
+    .s(s),
+    .en(en),
+    .d(d_golden)
+);
 
-            en = 0;
-             
-            #10
+decoder decoder_test(
+    .s(s),
+    .en(en),
+    .d(d_test)
+);
 
-            if (d_test == d_golden) begin
-                $display("OK: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
-            end 
-            else 
-            begin
-                $display("Fail: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
-                $display("Expect: s=%b, d=%b, en=%b, i=%0d", s, d_golden, en, i);
-            end
-         end
-         #10 $stop;
-    end
+
+initial begin
+    for(i = 0; i < 8; i = i + 1) begin
+        s = i;
+        en = 1;
+        
+        #10
+
+        if (d_test == d_golden) 
+        begin
+            $display("OK: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
+        end 
+        else 
+        begin
+            $display("Fail: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
+            $display("Expect: s=%b, d=%b, en=%b, i=%0d", s,d_golden,en,i);
+        end
+
+        en = 0;
+         
+        #10
+
+        if (d_test == d_golden) 
+        begin
+            $display("OK: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
+        end 
+        else 
+        begin
+            $display("Fail: s=%b, d=%b, en=%b, i=%0d", s, d_test, en, i);
+            $display("Expect: s=%b, d=%b, en=%b, i=%0d", s,d_golden,en,i);
+        end
+     end
+     #10 $stop;
+end
+
 endmodule
 
--- lab-1/Vivado/lab-1.srcs/sources_1/new/decoder.v
+++ lab-1/Vivado/lab-1.srcs/sources_1/new/decoder.v
@@ -20,38 +20,38 @@ module decoder(
     
     nand(w_1[5], not_s[2], not_s[1]);
     nand(not_w_1[5], w_1[5], w_1[5]);
-    
+        // row 1
         nand(w_2[7], not_w_1[5], not_s[0]);
         nand(not_w_2[7], w_2[7], w_2[7]);
         nand(w_3[7], not_w_2[7], en);
         nand(d[0], w_3[7], w_3[7]);
-        
+        // row 2
         nand(w_2[6], not_w_1[5], s[0]);
         nand(not_w_2[6], w_2[6], w_2[6]);
         nand(w_3[6], not_w_2[6], en);
         nand(d[1], w_3[6], w_3[6]);
-    
+    // row 3
     nand(w_1[4], not_s[2], not_s[0]);
     nand(not_w_1[4], w_1[4], w_1[4]);
     nand(w_2[5], not_w_1[4], s[1]);
     nand(not_w_2[5], w_2[5], w_2[5]);
     nand(w_3[5], not_w_2[5], en);
     nand(d[2], w_3[5], w_3[5]);
-    
+    // row 4
     nand(w_1[3], s[1], s[0]);
     nand(not_w_1[3], w_1[3], w_1[3]);
-    nand(w_2[4], not_w_1[3], not_s[2]); //
+    nand(w_2[4], not_w_1[3], not_s[2]);
     nand(not_w_2[4], w_2[4], w_2[4]);
     nand(w_3[4], not_w_2[4], en);
     nand(d[3], w_3[4], w_3[4]);
-    
+    // row 5
     nand(w_1[2], not_s[1], not_s[0]);
     nand(not_w_1[2], w_1[2], w_1[2]);
     nand(w_2[3], not_w_1[2], s[2]);
     nand(not_w_2[3], w_2[3], w_2[3]);
     nand(w_3[3], not_w_2[3], en);
     nand(d[4], w_3[3], w_3[3]);
-    
+    // row 6
     nand(w_1[1], s[2], s[0]);
     nand(not_w_1[1], w_1[1], w_1[1]);
     nand(w_2[2], not_w_1[1], not_s[1]);
@@ -61,12 +61,12 @@ module decoder(
     
     nand(w_1[0], s[2], s[1]);
     nand(not_w_1[0], w_1[0], w_1[0]);
-        
+        // row 7
         nand(w_2[1], not_w_1[0], not_s[0]);
         nand(not_w_2[1], w_2[1], w_2[1]);
         nand(w_3[1], not_w_2[1], en);
         nand(d[6], w_3[1], w_3[1]);
-        
+        //row 8
         nand(w_2[0], not_w_1[0], s[0]);
         nand(not_w_2[0], w_2[0], w_2[0]);
         nand(w_3[0], not_w_2[0], en);
