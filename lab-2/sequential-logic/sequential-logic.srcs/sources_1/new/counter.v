010b3538080cd1defd2e22eae480b64fd1bcf909
e1turin <ivturnik2003@gmail.com>
2024-04-08 14:16:42 +0300

    lab-2. Refactor rename module & add counter set signal

--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/counter.v
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/counter.v
@@ -1,15 +1,19 @@
 `timescale 1ns / 1ps
 
-module counter_div5
+module counter
 (
   input   logic       clk,
-  input   logic       rst, en,
+  input   logic [2:0] cin,
+  input   logic       set, en,
+  input   logic       rst, // async
   output  logic [2:0] cout
 );
 
-always @(posedge clk, posedge reset) 
+always @(posedge clk, posedge rst) 
 begin
-  if (rst | (cout == 5)) 
+  if (set & cin <= 5)
+    cout <= cin;
+  else if (rst | (cout == 5)) // should it be '>=5' ?
     cout <= 0;
   else if (en)      
     cout <= cout + 1;
