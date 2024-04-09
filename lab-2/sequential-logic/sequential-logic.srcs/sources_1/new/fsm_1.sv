19da88b7511add2006f31c254f26a13f75fe0dea
e1turin <ivturnik2003@gmail.com>
2024-04-09 16:37:02 +0300

    lab-2. Fix enum size

--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/fsm_1.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/fsm_1.sv
@@ -16,7 +16,7 @@ module fsm_1
 // ^------MUL_1-------^   ^-------MUL_2------^ : S_MUL
 // ^------------------SUM_2------------------^ : S_RES
 
-typedef enum logic [1:0] {
+typedef enum logic [2:0] {
   S_IDLE, // waiting for input
   S_DIV, S_SUM, S_MUL, S_RES
 } statetype;
@@ -30,17 +30,17 @@ always @(posedge clk, posedge reset) begin
   if (reset) state = S_DIV;
   else
     case (state)
-      //S_IDLE: state = S_DIV;
+      S_IDLE: state = S_DIV;
       S_DIV:  state = S_SUM;
       S_SUM:  state = S_MUL;
       S_MUL:  state = S_RES;
-      S_RES:  state = S_DIV;
+      S_RES:  state = S_IDLE;
     endcase
 end
 
 always @(posedge clk) begin
   case (state)
-    //S_IDLE: 
+    S_IDLE: ;
     S_DIV:  div_res <= {a >> 1, b >> 1};
     S_SUM:  sum_res <= {div_res[1] + b, div_res[2] + ~a + 1};
     S_MUL:  mul_res <= {sum_res[1] << 3, sum_res[2] << 2};
