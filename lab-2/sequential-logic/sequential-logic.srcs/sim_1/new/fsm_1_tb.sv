e9074ce3031f51190f2af311dea5a47e3d8f1107
e1turin <ivturnik2003@gmail.com>
2024-05-04 01:09:10 +0300

    test & enhance FSM

--- lab-2/sequential-logic/sequential-logic.srcs/sim_1/new/fsm_1_tb.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sim_1/new/fsm_1_tb.sv
@@ -3,49 +3,54 @@
 
 module fsm_1_tb;
   
-  localparam N = 16;
+  localparam N = 8;
   
   logic clk;
   logic reset;
   
-  logic [N-1:0] a_test;
-  logic [N-1:0] b_test;
-  logic       valid_test;
-  logic [N-1:0] result_test;
-  
+  logic   [N-1:0] test_a = $random();
+  logic   [N-1:0] test_b = $random();
+  logic           test_valid;
+  logic   [N-1:0] test_result;
+  integer         expect_result = (test_a/2 + test_b)*8 + (test_a - test_b/2)*4;
+  integer         expect_result_rem = expect_result % 2**N;
+
   fsm_1 # (
-    .N ( N )
+    .BIT_DEPTH ( N )
   ) dut (
     .clk    ( clk         ),
     .reset  ( reset       ),
-    .a      ( a_test      ), 
-    .b      ( b_test      ),
-    .valid  ( valid_test  ),
-    .result ( result_test )
+    .a      ( test_a      ), 
+    .b      ( test_b      ),
+    .valid  ( test_valid  ),
+    .result ( test_result )
   );
-  
-  always #20 clk = ~clk;
-  
+    
+  //  always #10 clk = ~clk
+
   initial 
   begin
     clk = 0;
     reset = 1;
-    #25
-    reset = 0;
-
-    a_test = 16'd2;
-    b_test = 16'd2;
-    valid_test = 1;
-//    clk = 0;
-//    for (integer i = 0; i < 9; i = i + 1)
-//    begin
-//      #10;
-//      clk = 1;
-//      #10;
-//      clk = 0;
-//    end
+    #10 reset = 0;
+    #10 test_valid = 1;
     
-    $display("result = %0d", result_test);
+    for (integer i = 0; i < 10; i = i + 1)
+    begin
+      #10 clk = 1;
+      #10 clk = 0;
+    end
+    if (test_result != expect_result % 2**N) 
+    begin
+      $error("result = %0d", test_result);
+      $error(
+        "expect result = %0d = %0d mod %0d",
+        expect_result, 
+        expect_result % 2**N, 
+        2**N
+      );
+    end
+//    $stop;
   end
 
 endmodule
--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/fsm_1.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/fsm_1.sv
@@ -1,64 +1,53 @@
 `timescale 1ns / 1ps
 
-module adder # (
-  parameter N = 8
+module explicit_adder # (
+  parameter BIT_DEPTH = 8
 ) (
-//  input   logic         clk,
-  input   logic [N-1:0] a,
-  input   logic [N-1:0] b,
-  output  logic [N-1:0] res
+  input   logic [BIT_DEPTH-1:0] a,
+  input   logic [BIT_DEPTH-1:0] b,
+  output  logic [BIT_DEPTH-1:0] res
 );
-  
-//  always_ff @(posedge clk)
-//    res <= a + b;
-  always_comb
+
+  always_comb begin
     res = a + b;
+  end
 
 endmodule
 
-module multiplier # (
-  parameter N = 2
+module explicit_multiplier # (
+  parameter BIT_DEPTH = 8
 ) (
-//  input   logic         clk,
-  input   logic [N-1:0] a,
-  output  logic [N-1:0] res
+  input   logic [BIT_DEPTH-1:0] a,
+  output  logic [BIT_DEPTH-1:0] res
 );
 
-//  always_ff @(posedge clk)
-//    res <= {a[N-2:0], 1'd0};
-
-  always_comb
-//    res = a << 1;
-    res = {a[N-2:0], 1'd0};
-
+  always_comb begin
+    res = a << 1;
+  end
     
 endmodule
 
-module divider # (
-  parameter N = 8
+module explicit_divider # (
+  parameter BIT_DEPTH = 8
 ) (
-//  input   logic         clk,
-  input   logic [N-1:0] a,
-  output  logic [N-1:0] res
+  input   logic [BIT_DEPTH-1:0] a,
+  output  logic [BIT_DEPTH-1:0] res
 );
   
-//  always_ff @(posedge clk)
-//    res <= {1'd0, a[N-1:1]};
-  always_comb
-//    res = a >> 1;
-    res = {1'd0, a[N-1:1]};
-
+  always_comb begin
+    res = a >> 1;
+  end
     
 endmodule
 
 module fsm_1 # (
-  parameter N = 8
+  parameter BIT_DEPTH = 8
 ) (
-  input   logic         clk,
-  input   logic         reset, // async
-  input   logic [N-1:0] a, b,
-  input   logic         valid,
-  output  logic [N-1:0] result
+  input   logic                 clk,
+  input   logic                 reset, // async
+  input   logic [BIT_DEPTH-1:0] a, b,
+  input   logic                 valid,
+  output  logic [BIT_DEPTH-1:0] result
 );
 
 // (A/2+B)*8 + (A-B/2)*4 = ((A>>1) + B)<<3 + (A + ~(B>>1) + 1)<<2
@@ -74,48 +63,47 @@ module fsm_1 # (
 // |-------------------5+--------------------------| : S5   : S5
 //  put out result                                   : S6
 
-  typedef enum logic [3:0] {
+  typedef enum logic [3:0] { // 10 states at all
     S0, // waiting for input
     S1,   
     S2, 
     S3_0, S3_1,
     S4_0, S4_1, S4_2, 
     S5,
-    S6
+    S6 // return the result
   } state_e;
   
   state_e curr_state = S0;
   state_e next_state = S0;
   
-  logic [N-1:0] div_res;
-  logic [N-1:0] sum_res;
-  logic [N-1:0] mul_res;
+  logic [BIT_DEPTH-1:0] adder_a, adder_b;
+  logic [BIT_DEPTH-1:0] sum_res;
+
+  logic [BIT_DEPTH-1:0] divider_2_a;
+  logic [BIT_DEPTH-1:0] div_res;
   
-  logic [N-1:0] adder_a, adder_b;
-  logic [N-1:0] multiplier_2_a;
-  logic [N-1:0] divider_2_a;
+  logic [BIT_DEPTH-1:0] multiplier_2_a;
+  logic [BIT_DEPTH-1:0] mul_res;
   
-  adder # ( 
-    .N ( N )
+  
+  explicit_adder # ( 
+    .BIT_DEPTH ( BIT_DEPTH )
   ) adder_1 (
-//    .clk ( clk     ),
     .a   ( adder_a ),
     .b   ( adder_b ),
     .res ( sum_res )
   );
   
-  multiplier # ( 
-    .N ( N )
+  explicit_multiplier # ( 
+    .BIT_DEPTH ( BIT_DEPTH )
   ) multiplier_4_1 (
-//    .clk ( clk            ),
     .a   ( multiplier_2_a ),
     .res ( mul_res        )
   );
   
-  divider # (
-    .N ( N )
+  explicit_divider # (
+    .BIT_DEPTH ( BIT_DEPTH )
   ) divider_2_1 (
-//    .clk ( clk         ),
     .a   ( divider_2_a ),
     .res ( div_res     )
   );
@@ -151,48 +139,37 @@ module fsm_1 # (
           divider_2_a <= b;
           adder_a <= a;
           adder_b <= 1;
-//          result <= 1;
         end
         S2 : begin
           divider_2_a <= a;
           adder_a <= sum_res;
-          adder_b <= ~div_res; // <- important inversion
-//          result <= 2;
+          adder_b <= ~div_res;
         end
         S3_0 : begin
           adder_a <= div_res;
           adder_b <= b;
           multiplier_2_a <= sum_res;
-//          result <= 30;
         end
         S3_1 : begin
           multiplier_2_a <= mul_res;
-//          result <= 31;
         end
         S4_0 : begin
           adder_a <= mul_res;
           adder_b <= 0;
           multiplier_2_a <= sum_res;
-//          result <= 40;
         end
         S4_1 : begin
           multiplier_2_a <= mul_res;
-//          result <= 41;
         end
         S4_2 : begin
           multiplier_2_a <= mul_res;
-//          result <= 42;
         end
         S5 : begin
           adder_a <= sum_res;
           adder_b <= mul_res;
-//          result <= 5;
         end
         S6 : begin
           result <= sum_res;
-        end
-        default : begin 
-//          result <= 8'd0;
         end
       endcase
   end
