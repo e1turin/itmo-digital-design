7f0fd9bfb4aec48b504a7e11aea64dc3005129cd
e1turin <ivturnik2003@gmail.com>
2024-05-06 18:24:32 +0300

    try to impl arbiter & additional comments

--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/arbiter.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/arbiter.sv
@@ -12,14 +12,81 @@ module arbiter # (
   parameter BIT_DEPTH = 8
 ) (
   input   logic   clk,
-  transaction_if  t_1_i, 
+  input   logic   arst,
+  output  logic   ready_o [3:0],
+  transaction_if  t_1_i,
   transaction_if  t_2_i,
   transaction_if  t_3_i,
   transaction_if  t_4_i,
   transaction_if  t_o
 );
+ 
+  typedef enum logic [1:0] {
+      IDLE
+    , SCHEDULE
+    , PASS
+  } state_e;
 
-  assign t_o.data = t_1_i.data;
-  assign t_o.valid = t_1_i.valid;
+  state_e current_state = IDLE;
+  state_e next_state    = IDLE;
+  
+  logic [3:0] req;
+  logic       req_status;
+  
+  assign req = {t_1_i.valid,
+                t_2_i.valid, 
+                t_3_i.valid,
+                t_4_i.valid};
+  assign req_status = | req;
+  
+  logic [3:0] grant;
+  
+  assign grant = {req[0], 
+                  ~req[0] & req[1], 
+                  ~req[0] & ~req[1] & req[2], 
+                  ~req[0] & ~req[1] & ~req[2] & req[3]};
+
+  // get next state
+  always_comb
+  begin
+    next_state = current_state;
+    case (current_state)
+      IDLE      : if (req_status)   next_state = SCHEDULE;
+      SCHEDULE  :                   next_state = PASS;
+      PASS      : if (!req_status)  next_state = SCHEDULE;
+      default: next_state = IDLE;
+    endcase
+  end
+  
+  // update state
+  always_ff @(posedge clk or posedge arst)
+  begin
+    if (arst) current_state <= IDLE;
+    else      current_state <= next_state;
+  end
+  
+  // define output value
+  always_comb
+  begin
+    case (current_state)
+      IDLE: begin
+        ready_o = {'d1, 'd1, 'd1, 'd1};
+      end
+      SCHEDULE: begin
+        ready_o = {'d0};
+        t_o.valid = 'd0;
+        case (grant)
+          4'b0001: t_o.data = t_1_i.data;
+          4'b0010: t_o.data = t_2_i.data;
+          4'b0100: t_o.data = t_3_i.data;
+          4'b1000: t_o.data = t_4_i.data;
+        endcase
+      end
+      PASS: begin
+        t_o.valid = 'd1;
+      end
+      default: ;
+    endcase
+  end
 
 endmodule
