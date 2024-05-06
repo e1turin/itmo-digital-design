be8ae01aaacde405b60e6bbe39f9f9279cc48a3c
e1turin <ivturnik2003@gmail.com>
2024-05-06 03:19:28 +0300

    test shiftreg basic operation

---
+++ lab-2/sequential-logic/sequential-logic.srcs/sim_1/new/shiftreg_tb.sv
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+
+module shiftreg_tb;
+
+  localparam N = 8;
+
+  logic clk = 0;
+  logic rst = 0;
+
+  logic         test_enable;
+  logic         test_load = 0;
+  logic         test_shift_input;
+  logic [N-1:0] test_data_input;
+  logic [N-1:0] test_data_output;
+  logic         test_shift_output;
+
+  shiftreg # (
+    .BIT_DEPTH ( N )
+  ) sr (
+    .clk      ( clk ),
+    .arst     ( rst ),  
+    .enable   ( test_enable ),
+    .load_i   ( test_load ),
+    .shift_i  ( test_shift_input ),
+    .data_i   ( test_data_input ),
+    .data_o   ( test_data_output ),
+    .shift_o  ( test_shift_output )
+  );
+  
+  always #10 clk = ~clk;
+  
+  logic [N-1:0] data = 'd6;
+  logic [N-1:0] init_data = data;
+
+  
+  initial
+  begin
+    #15 rst = 1;
+    #5;
+    if (test_data_output) $error("Data output not cleared");
+
+    #10 rst = 0;
+    
+    #5 test_enable = 'd1;
+    
+    repeat(N)
+    begin
+      @(posedge clk);
+      test_shift_input = data[N-1];
+      #5 data <<= 1;
+    end
+    
+    #5 test_enable = 'd0;
+    
+    #15;
+    
+    if (test_data_output != init_data) $error("Incorrect reading");
+    
+    #20;
+    
+    $finish;
+  end
+  
+
+endmodule
--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/shiftreg.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/shiftreg.sv
@@ -4,21 +4,25 @@ module shiftreg
 #(
   parameter BIT_DEPTH = 8
 ) (
-  input   logic                 clk_i,
-  input   logic                 reset_i, load_i,
+  input   logic                 clk,
+  input   logic                 arst, 
+  input   logic                 enable,
+  input   logic                 load_i,
   input   logic                 shift_i,
   input   logic [BIT_DEPTH-1:0] data_i,
   output  logic [BIT_DEPTH-1:0] data_o,
   output  logic                 shift_o
 );
 
-assign shift_i = data_o[BIT_DEPTH-1];
+  assign shift_o = data_o[BIT_DEPTH-1];
 
-always_ff @(posedge clk_i, posedge reset_i) begin
-  if (reset_i) data_o <= 0;
-  else 
-    if (load_i) data_o <= data_i;
-    else        data_o <= {data_o[BIT_DEPTH-2:0], shift_i};
-end
+  always_ff @(posedge clk or posedge arst) 
+  begin
+    if (arst) data_o <= 0;
+    else 
+      if (enable)
+        if (load_i) data_o <= data_i;
+        else        data_o <= {data_o[BIT_DEPTH-2:0], shift_i};
+  end
 
 endmodule
