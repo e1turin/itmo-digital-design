40d923a3ab1d3ae79cc3a31d6c3f690aa83000d9
e1turin <ivturnik2003@gmail.com>
2024-05-06 20:07:17 +0300

    complete arbiter with no fsm

--- lab-2/sequential-logic/sequential-logic.srcs/sim_1/new/arbiter_tb.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sim_1/new/arbiter_tb.sv
@@ -3,31 +3,89 @@
 
 module arbiter_tb;
 
-  localparam N = 32; 
+  localparam N = 8; 
   
-  logic [N-1:0] data_i;
-  logic [N-1:0] data_o;
-  logic clk;
+  logic clk = 0;
+  logic arst = 'd0;
+  logic ready;
 
-  transaction_if # ( .BIT_DEPTH ( N ) )  t_i();
+  transaction_if # ( .BIT_DEPTH ( N ) )  t_1();
+  transaction_if # ( .BIT_DEPTH ( N ) )  t_2();
+  transaction_if # ( .BIT_DEPTH ( N ) )  t_3();
+  transaction_if # ( .BIT_DEPTH ( N ) )  t_4();
+
   transaction_if # ( .BIT_DEPTH ( N ) )  t_o();
 
-  arbiter # (
+  logic [N-1:0] test_data_out;
+  assign test_data_out = t_o.data;
+
+  logic test_valid_out;
+  assign test_valid_out = t_o.valid;
+
+  arbiter # ( 
     .BIT_DEPTH ( N )
-  ) arb ( 
-     .clk ( clk ),
-     .t_1_i ( t_i ),
-     .t_2_i ( t_i ),
-     .t_3_i ( t_i ),
-     .t_4_i ( t_i ),
-     .t_o ( t_o )
+  ) dut ( 
+     .clk     ( clk   ),
+     .arst    ( arst  ),
+     .ready_o ( ready ),
+     .t_1_i   ( t_1.RCV ),
+     .t_2_i   ( t_2.RCV ),
+     .t_3_i   ( t_3.RCV ),
+     .t_4_i   ( t_4.RCV ),
+     .t_o     ( t_o.SND )
   );
-  integer data = $random();
+
+  logic [N-1:0] test_data_1 = 10;
+  logic [N-1:0] test_data_2 = 11;
+  logic [N-1:0] test_data_3 = 12;
+  logic [N-1:0] test_data_4 = 13;
+  
+  always #10 clk = ~clk;
+
   initial
   begin
-    #10 t_i.data = data;
-    #10 t_i.valid = 1;
-    #10 $display("%0d = %0d and %0d", data, t_o.data, t_o.valid);
+    t_1.valid = 'd0;
+    t_2.valid = 'd0;
+    t_3.valid = 'd0;
+    t_4.valid = 'd0;
+    #15 arst = 'd1;
+    #10  arst = 'd0;
+    #5;
+    if (!ready) $error("Arbiter not ready after reset");
+
+    #10;
+    t_1.data = test_data_1;
+    t_2.data = test_data_2;
+    t_3.data = test_data_3;
+    t_4.data = test_data_4;
+    
+    #5;
+    t_1.valid = 'd1;
+    t_2.valid = 'd1;
+
+    #5;
+    if (ready) $error("Arbiter ready when transaction exists");
+    
+    repeat(2)
+    begin
+      @(posedge clk);
+      $display("data out = %0d and valid = %0d", test_data_out, test_valid_out);
+    end
+    
+    #5;
+    t_1.valid = 'd0;
+    t_2.valid = 'd1;
+    t_3.valid = 'd1;
+    t_4.valid = 'd1;
+    
+    repeat(3)
+    begin
+      @(posedge clk);
+      $display("data out = %0d and valid = %0d", test_data_out, test_valid_out);
+    end
+    
+    #20;
+//    #10 $display("%0d = %0d and %0d", test_data, test_data_out, test_valid_out);
     $finish;
   end
 
--- lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/arbiter.sv
+++ lab-2/sequential-logic/sequential-logic.srcs/sources_1/new/arbiter.sv
@@ -5,6 +5,10 @@ interface transaction_if # (
 );
   logic [BIT_DEPTH-1:0] data;
   logic                 valid;
+
+  modport SND (input  data, input   valid);
+  modport RCV (output data, output  valid);
+
 endinterface
 
 
@@ -13,80 +17,129 @@ module arbiter # (
 ) (
   input   logic   clk,
   input   logic   arst,
-  output  logic   ready_o [3:0],
+  output  logic   ready_o,
   transaction_if  t_1_i,
   transaction_if  t_2_i,
   transaction_if  t_3_i,
   transaction_if  t_4_i,
   transaction_if  t_o
 );
- 
-  typedef enum logic [1:0] {
-      IDLE
-    , SCHEDULE
-    , PASS
-  } state_e;
 
-  state_e current_state = IDLE;
-  state_e next_state    = IDLE;
+  logic [3:0] req = 'd0;  
+  logic valid_1;
+  logic valid_2;
+  logic valid_3;
+  logic valid_4;
   
-  logic [3:0] req;
-  logic       req_status;
-  
-  assign req = {t_1_i.valid,
-                t_2_i.valid, 
-                t_3_i.valid,
-                t_4_i.valid};
+  assign valid_1 = t_1_i.valid;
+  assign valid_2 = t_2_i.valid;
+  assign valid_3 = t_3_i.valid;
+  assign valid_4 = t_4_i.valid;
+
+  logic  req_status;
   assign req_status = | req;
   
-  logic [3:0] grant;
-  
-  assign grant = {req[0], 
-                  ~req[0] & req[1], 
-                  ~req[0] & ~req[1] & req[2], 
-                  ~req[0] & ~req[1] & ~req[2] & req[3]};
+  logic [1:0] grant_ptr;
 
-  // get next state
   always_comb
   begin
-    next_state = current_state;
-    case (current_state)
-      IDLE      : if (req_status)   next_state = SCHEDULE;
-      SCHEDULE  :                   next_state = PASS;
-      PASS      : if (!req_status)  next_state = SCHEDULE;
-      default: next_state = IDLE;
+    casez (req)
+      4'b???1: grant_ptr = 'd0;
+      4'b??10: grant_ptr = 'd1;
+      4'b?100: grant_ptr = 'd2;
+      4'b1000: grant_ptr = 'd3;      
     endcase
   end
-  
-  // update state
+
   always_ff @(posedge clk or posedge arst)
   begin
-    if (arst) current_state <= IDLE;
-    else      current_state <= next_state;
+    if (arst || !req_status)
+    begin
+      req <= {valid_4,
+              valid_3, 
+              valid_2,
+              valid_1};
+      ready_o <= 'd1;
+    end
+    else
+    begin
+      req[grant_ptr] = 'd0;
+      ready_o <= 'd0;
+    end
   end
-  
-  // define output value
+
   always_comb
   begin
-    case (current_state)
-      IDLE: begin
-        ready_o = {'d1, 'd1, 'd1, 'd1};
-      end
-      SCHEDULE: begin
-        ready_o = {'d0};
-        t_o.valid = 'd0;
-        case (grant)
-          4'b0001: t_o.data = t_1_i.data;
-          4'b0010: t_o.data = t_2_i.data;
-          4'b0100: t_o.data = t_3_i.data;
-          4'b1000: t_o.data = t_4_i.data;
-        endcase
-      end
-      PASS: begin
-        t_o.valid = 'd1;
-      end
-      default: ;
-    endcase
+    if (req_status)
+    begin
+      t_o.valid = 'd1;
+      case (grant_ptr)
+        2'd0: t_o.data = t_1_i.data;
+        2'd1: t_o.data = t_2_i.data;
+        2'd2: t_o.data = t_3_i.data;
+        2'd3: t_o.data = t_4_i.data;
+      endcase
+    end
+    else t_o.valid = 'd0;
   end
+  
+   
+//  typedef enum logic [1:0] {
+//      IDLE
+//    , SCHEDULE
+//    , PASS
+//  } state_e;
+
+//  state_e current_state = IDLE;
+//  state_e next_state    = IDLE;
+  
+//  assign grant = {req[0], 
+//                  ~req[0] & req[1], 
+//                  ~req[0] & ~req[1] & req[2], 
+//                  ~req[0] & ~req[1] & ~req[2] & req[3]};
+
+//  // get next state
+//  always_comb
+//  begin
+//    next_state = current_state;
+//    case (current_state)
+//      IDLE      : if (req_status)   next_state = SCHEDULE;
+//      SCHEDULE  :                   next_state = PASS;
+//      PASS      : if (!req_status)  next_state = SCHEDULE;
+//      default: next_state = IDLE;
+//    endcase
+//  end
+
+  
+//  // update state
+//  always_ff @(posedge clk or posedge arst)
+//  begin
+//    if (arst) current_state <= IDLE;
+//    else      current_state <= next_state;
+//  end
+  
+//  // define output value
+//  always_comb
+//  begin
+//    case (current_state)
+//      IDLE: begin
+//        ready_o = {'d1, 'd1, 'd1, 'd1};
+//      end
+//      SCHEDULE: begin
+//        ready_o = {'d0};
+//        t_o.valid = 'd0;
+//        case (grant)
+//          4'b0001: t_o.data = t_1_i.data;
+//          4'b0010: t_o.data = t_2_i.data;
+//          4'b0100: t_o.data = t_3_i.data;
+//          4'b1000: t_o.data = t_4_i.data;
+//        endcase
+//      end
+//      PASS: begin
+//        t_o.valid = 'd1;
+//      end
+//      default: ;
+//    endcase
+//  end
 
 endmodule
